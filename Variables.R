735182d795c1c44160f6356b96195792ca805e47
Lucas Lima <lllima3780@gmail.com>
2023-08-22 21:19:33 -0300

    Linguagem R

---
+++ Variables.R
@@ -0,0 +1,23 @@
+#VARIABLES DEVE DAR NOME A VARIAVEL E ATRIBUIR SUA FUNCAO (<- simbolo de operador de funcao)
+
+variable <- 100
+
+#PODEMOS CHAMAR A VARIAVEL E MOSTRARA O QUE ELA COMPORTA
+variable
+bank <- 1000
+
+#PODEMOS USAR "." PARA SEPARAR OS NOMES ENTRE A VARIÁVEL
+bank.account <- 500
+
+#PODEMOS USAR CAMELCASE 
+bankAccount <- 500
+
+# SE USAR SNAKECASE FUNCIONA MAS É MAL USO
+bank_account <- 500
+
+#EXEMPLO DE CALCULO COM VARIAVEIS (O sistema vai somar mais 20)
+deposit <- 20
+bank.account <- bank.account + deposit
+bank.account
+
+
